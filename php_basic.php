9d73f5bc3dc8e1bbf19d5a721042aca8e7e44e92
ari_fakhri_wilaga <arifakhri97@gmail.com>
2016-09-09 06:57:33 +0700

    neat the code

--- php_basic.php
+++ php_basic.php
@@ -53,7 +53,7 @@ echo "
 fscanf(STDIN, "%s\n", $game_play);
 echo"\n";
 if ($game_play =="new") 
-{
+  {
 echo " 
 # Current Player: #
 # - #
@@ -63,8 +63,8 @@ echo "
 fscanf(STDIN, "%s\n", $much_players);
 echo"\n";
 // $players [$much_players] = new Player($much_players);
-if ($much_players <= 3 && $much_players >=2 ) 
-{
+  if ($much_players <= 3 && $much_players >=2 ) 
+    {
 echo " 
 # ============================== #
 # Welcome to the Battle Arena #
@@ -72,73 +72,73 @@ echo "
 # Description: #
 # Current Player : " . $much_players . "         
 # ------------------------------------------------- ---- #";
-for ($i=0; $i < $much_players ; $i++) 
-{ 
+  for ($i=0; $i < $much_players ; $i++) 
+    { 
 echo"\n";
 echo "# Put Player Name:";
 fscanf(STDIN, "%s\n", $player_name);
 $players [$player_name] = new Player($player_name);
 echo "var_dump($player_name)";
-}
+    }
+
 echo"\n";
 echo "Start now?";
 fscanf(STDIN, "%s\n", $mode);
-if ($mode == "Start") 
-{
-  echo"\n";
-  echo " 
-    # Attack:";
+  if ($mode == "Start") 
+  {
+    echo"\n";
+    echo " 
+# Attack:";
     fscanf(STDIN, "%s\n", $attacker);
-foreach ($players as $key => $value) 
-{
-  if ($attacker == $value->get_name()) 
-  {
-    $value->get_manna();
-    $value->attack();
-  }
-}
-
-  echo"\n";
-  echo " 
-    # Defend:";
+      foreach ($players as $key => $value) 
+        {
+          if ($attacker == $value->get_name()) 
+            {
+              $value->get_manna();
+              $value->attack();
+            }
+        }
+    echo"\n";
+    echo " 
+# Defend:";
     fscanf(STDIN, "%s\n", $defender);
-foreach ($players as $key => $value) 
-{
-  if ($defender == $value->get_name()) 
-  {
-    $value->get_blood();
-    $value->defend();
-  echo"\n";
-  echo "Attacker :". $attacker ;
-  echo "Defender :". $defender;
-  echo"\n";
+      foreach ($players as $key => $value) 
+      {
+        if ($defender == $value->get_name()) 
+          {
+            $value->get_blood();
+            $value->defend();
+echo"\n";
+echo "Attacker :". $attacker ;
+echo "Defender :". $defender;
+echo"\n";
+          }
+      } 
+
+      foreach ($players as $key => $value) 
+      {
+        echo $value->get_name() .", manna : " .$value->get_manna() .", blood : " .$value->get_blood();
+        echo"\n";
+      }           
   }
-} 
 
-foreach ($players as $key => $value) 
-{
-  echo $value->get_name() .", manna : " .$value->get_manna() .", blood : " .$value->get_blood();
-  echo"\n";
-}           
+else
+  {
+    echo "Incorrect type";
+  }
+  
+    }
+
+  else
+    {
+      echo "Incorrect";
+    }
 }
 
 else
-{
-echo "Incorrect type";
-}
-
-}
-else
-{
-echo "Incorrect";
-}
-
-}
-
-else
-{
-echo "Incorrect mode";
-}
+  {
+    echo "Incorrect mode";
+  }
 
 ?>
 
